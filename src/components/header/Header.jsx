5ffa5908c117ad529c18d118b718f9fb200a8050
devdashk66 <devdashk66@gmail.com>
2024-07-09 20:08:27 +0600

    mobile menu fixed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -21,7 +21,7 @@ const Header = () => {
         <button
           onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
           type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-end text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          className="inline-flex items-center p-2 w-10 h-10 justify-end text-sm text-gray-200 rounded-lg lg:hidden  focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           aria-controls="navbar-multi-level"
         >
           <span className="sr-only">Open main menu</span>
@@ -29,7 +29,7 @@ const Header = () => {
         </button>
         <div className={`w-full lg:block lg:w-auto`}>
           <ul
-            className={`bg-white absolute top-0 h-screen lg:h-fit ${
+            className={`bg-gray-800 absolute top-0 h-screen lg:h-fit ${
               isMobileMenuOpen ? "left-0" : "-left-96"
             } py-10 lg:py-0 px-10 lg:px-0  h-full flex-col lg:flex-row  lg:static lg:bg-transparent flex gap-5 lg:gap-14 shadow-2xl lg:shadow-none font-semibold text-white duration-200`}
           >
